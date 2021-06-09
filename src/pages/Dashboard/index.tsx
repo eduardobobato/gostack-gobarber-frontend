5f5ce1677838fea38910719be4cca01aca51bdcc
Eduardo Bobato <eduardobobato@hotmailcom.br>
2021-06-09 01:58:52 -0300

    Componente de atendimento a seguir

--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -1,7 +1,16 @@
 import React from 'react';
 
-import { FiPower } from 'react-icons/fi';
-import { Container, Header, HeaderContent, Profile } from './styles';
+import { FiClock, FiPower } from 'react-icons/fi';
+import {
+  Container,
+  Header,
+  HeaderContent,
+  Profile,
+  Content,
+  Schedule,
+  Calendar,
+  NextAppointments,
+} from './styles';
 
 import logoImg from '../../assets/logo.svg';
 import { useAuth } from '../../hooks/auth';
@@ -25,6 +34,32 @@ const Dashboard: React.FC = () => {
           </button>
         </HeaderContent>
       </Header>
+
+      <Content>
+        <Schedule>
+          <h1>Horarios Agendados</h1>
+          <p>
+            <span>Hoje</span>
+            <span>Dia 6</span>
+            <span>Segunda-Feira</span>
+          </p>
+          <NextAppointments>
+            <strong>Atendimentos a seguir</strong>
+            <div>
+              <img
+                src="https://avatars.githubusercontent.com/u/34967841?v=4"
+                alt="Bobato"
+              />
+              <strong>Bobato</strong>
+              <span>
+                <FiClock />
+                08:00
+              </span>
+            </div>
+          </NextAppointments>
+        </Schedule>
+        <Calendar />
+      </Content>
     </Container>
   );
 };
